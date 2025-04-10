d82f5639ed657d83e025a4eb7c57a23c603306f9
nickperi <137534291+nickperi@users.noreply.github.com>
2025-04-09 21:48:37 -0400

    Update auth.py

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -5,7 +5,7 @@ from App.models import User
 def login(username, password):
   user = User.query.filter_by(username=username).first()
   if user and user.check_password(password):
-    return create_access_token(identity=username)
+    return create_access_token(identity=user.id)
   return None
 
 
@@ -17,13 +17,13 @@ def setup_jwt(app):
   def user_identity_lookup(identity):
     user = User.query.filter_by(username=identity).one_or_none()
     if user:
-        return user.username
+        return user.id
     return None
 
   @jwt.user_lookup_loader
   def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
-    return User.query.filter_by(username=identity).one_or_none()
+    return User.query.get(identity)
   return jwt
 
 
