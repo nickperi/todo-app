0a8c1667d0d80ef20a84fa0e8f06eb78c828b07f
nickperi <nicholaspariag27@gmail.com>
2025-08-01 04:11:19 -0400

    edit task feature

--- App/controllers/todo.py
+++ App/controllers/todo.py
@@ -42,31 +42,3 @@ def get_all_todos_json():
     todos = [todo.get_json() for todo in todos]
     return todosfrom App.models import Todo, Student
 from App.database import db
-
-
-def create_todo(text, user_id):
-    student = Student.query.filter_by(studentid=user_id).first()
-
-    if not student:
-        return None
-    
-    todo = Todo.query.filter_by(user_id=user_id, text=text).first()
-
-    if not todo:
-        new_todo = student.add_todo(text=text)
-        return new_todo
-    return None
-
-
-def get_all_todos():
-    todos = Todo.query.all()
-    return todos
-
-def get_all_todos_json():
-    todos = Todo.query.all()
-
-    if not todos:
-        return []
-    
-    todos = [todo.get_json() for todo in todos]
-    return todos
