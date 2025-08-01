2570f15e6378bab9b5241a6a1626db24ebdce30d
nickperi <nicholaspariag27@gmail.com>
2025-08-01 05:14:38 -0400

    db migrations

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,3 @@
-SQLALCHEMY_DATABASE_URI="sqlite:///temp-database.db"
+#SQLALCHEMY_DATABASE_URI="sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI="postgresql://user:womUffHT9oGbucVBHN7s3fEjQ8D4LRCT@dpg-d2684663jp1c73cnf9h0-a.oregon-postgres.render.com/todo_db_x2dp"
 SECRET_KEY="secret key"
