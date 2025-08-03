406dff4630715613369830c40aefa7def905a1db
nickperi <nicholaspariag27@gmail.com>
2025-08-03 03:30:07 -0400

    update welcome page

--- App/views/index.py
+++ App/views/index.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.controllers import create_user, initialize
+from flask_jwt_extended import current_user
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
 def index_page():
-    return render_template('index.html')
+    return render_template('index.html', current_user=current_user)
 
 @index_views.route('/init', methods=['GET'])
 def init():
