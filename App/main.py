4b2c2169150e7694d70fa71c01226043b0a8aab4
nickperi <nicholaspariag27@gmail.com>
2025-09-14 13:57:35 -0400

    get json todos for previous and current months

--- App/main.py
+++ App/main.py
@@ -22,7 +22,7 @@ def add_views(app):
         app.register_blueprint(view)
 
 def create_app(overrides={}):
-    app = Flask(__name__, static_url_path='/static/dist')
+    app = Flask(__name__, static_url_path='/static')
     load_config(app, overrides)
     CORS(app, origins=["http://127.0.0.1:5000", "http://localhost:5173"], supports_credentials=True)
     add_auth_context(app)
